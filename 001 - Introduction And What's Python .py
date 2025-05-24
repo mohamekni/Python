4d15a263540df24ec28a4f8ffd556761553b3322
mohamekni <mekni.moha99@gmail.com>
2025-05-24 19:48:28 +0200

    Learn Python

---
+++ 001 - Introduction And What's Python .py
@@ -0,0 +1,32 @@
+# - What is Python ?
+# Python is a programming language.
+# Free and Open Source
+# Interpreted Language
+# Object Oriented Language
+# Easy to Install
+# Clean and Easy
+# Error Handling
+# Debugging is Easy
+# Cross Platform
+# Expensive
+# Integrated with Other Languages
+# Support Modules and Packages
+# Large Set of Libs and Plugins
+# Memory Management
+# Multi Purpose
+# Great Community
+# Growing Fast
+
+# What's Python Used For ?
+# Web Development (Django, Flask)
+# Machine Learning (TensorFlow, Keras, Scikit-Learn)
+# Artificial Intelligence (OpenCV, PyTorch)
+# Data Science (Pandas, NumPy, Matplotlib)
+# Desktop Applications (Tkinter, PyQt, Kivy)
+# Mobile Applications (Kivy, PyQt, Tkinter)
+# Web Scraping (Scrapy, BeautifulSoup)
+# Game Development (Pygame, Panda3D)
+# Automation (Selenium, PyAutoGUI)
+# Scripting (Bash, PowerShell)
+# Cybersecurity (Scapy, PyCrypto)
+
---
+++ 002 - Syntax And Your First App.py
@@ -0,0 +1,8 @@
+if True:
+    print('Hello World!')
+    print('My Name is Mohamed Mekni')
+    
+if True:
+    print('I am an Engineer Student')
+    print('I am 22 years old')
+
