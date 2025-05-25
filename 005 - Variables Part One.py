d4ab316596ab9b105101ff4d10692766fc6ec9e4
mohamekni <mekni.moha99@gmail.com>
2025-05-25 02:48:24 +0200

    Learn Python

---
+++ 005 - Variables Part One.py
@@ -0,0 +1,21 @@
+# Variables
+# Syntax => [Variable Name] [Assignment Operator] [Value]
+# Name Convention and Rules
+# [1] Can Start With (a-z, A-Z, _Underscore)
+# [2] Cant't Start With Number or Special Characters
+# [3] Can Include (0-9) or Underscore
+# [4] Cannot Include Special Characters
+# [5] name is Not Like Name [Case Sensitive]
+
+_my7__Variable = "My Value"
+print(_my7__Variable)
+
+
+# Best Practice
+name = "MohaSoGoodNoLove" # Single Word => Normal
+fullName = "MohamedMekni" # Two Words => camelCase
+my_name = "Mohamed Mekni" # Two Words => snake_case
+
+print(name)
+print(fullName)
+print(my_name)
---
+++ 006 - Variables Part Two.py
@@ -0,0 +1,12 @@
+# Source Code : Original Code
+# Translation : Converting Source Code Into Machine Language
+# Compilation : Translation Code Before Execution
+# Interpretation : Translation Code During Execution
+# Runtime : Execution Time
+
+help("keywords") # Reserved Words In Python
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
