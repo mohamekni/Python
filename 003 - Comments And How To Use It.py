95818f9488afcc5106b6336b8bdde85a171f35b9
mohamekni <mekni.moha99@gmail.com>
2025-05-25 02:17:39 +0200

    Learn Python

---
+++ 003 - Comments And How To Use It.py
@@ -0,0 +1,10 @@
+print('Hello World') # This is Comment
+print('Programming') # I Used This Method For Print
+print('Python') # I Used Test Method Will Through Error
+
+"""
+This Is
+Multiple Line
+Comment
+
+"""
---
+++ 004 - Some Data Types Overview.py
@@ -0,0 +1,10 @@
+# Type ()
+# All Data in Python is Object
+
+print(type(10)) # int => Integer
+print(type(10.9)) # float => Floating Point Number
+print(type("Hello Python")) # str => String
+print(type( [1,2,3,4,5] )) # list => List
+print(type( (1,2,3,4,5) )) # tuple => Tuple
+print(type({"One" : 1, "Two" : 2, "Three" : 3})) # dict => Dictionary
+print(type(2 == 2)) # bool => Boolean
