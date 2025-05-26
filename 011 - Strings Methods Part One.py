dd84b2908349f4af1f8bcfd1644cd31441954381
mohamekni <mekni.moha99@gmail.com>
2025-05-26 23:16:01 +0200

    Learn Python

---
+++ 011 - Strings Methods Part One.py
@@ -0,0 +1,13 @@
+# Strings Methods
+
+# strip() rstrip() lstrip()
+A = "    Mohamed    Mekni    "
+print(A.strip())
+print(A.rstrip())
+print(A.lstrip())
+
+B = "###Mohamed###Mekni###"
+print(B.strip('#'))
+print(B.rstrip('#'))
+print(B.lstrip('#'))
+
