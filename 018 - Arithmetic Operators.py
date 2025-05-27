ab60ef85d8ad02213bf05d0a70480dc6066e032f
mohamekni <mekni.moha99@gmail.com>
2025-05-28 00:56:50 +0200

    Learn Python

---
+++ 018 - Arithmetic Operators.py
@@ -0,0 +1,56 @@
+# Arithmetic Operators
+
+# [+] Addition
+# [-] Subtraction
+# [*] Multiplication
+# [/] Division
+# [%] Modulus
+# [**] Exponentiation
+# [//] Floor division
+
+
+# [+] Addition
+print(10 + 5) # 15
+print(10.5 + 5.5) # 16.0
+print(-10 + 20) # 10
+print(9.9 + 9.9) # 19.8
+
+
+# [-] Subtraction
+print(10 - 5) # 5
+print(10.5 - 5.5) # 5.0
+print(-10 - 20) # -30
+print(9.9 - 9.9) # 0.0
+
+
+# [*] Multiplication
+print(10 * 5) # 50
+print(5 + 10 * 100) # 1005
+print((5 + 10) * 100) # 1500
+print(10.5 * 5.5) # 57.75
+
+
+# [/] Division
+print(10 / 5) # 2.0
+print(int(10 / 5)) # 2
+
+
+# [%] Modulus
+print(10 % 5) # 0
+print(10 % 3) # 1
+print(10 % 2) # 0
+print(10 % 7) # 3
+
+
+# [**] Exponentiation
+print(10 ** 2) # 100
+print(10 ** 3) # 1000
+print(10 ** 4) # 10000
+print(10 ** 5) # 100000
+
+
+# [//] Floor division
+print(10 // 3) # 3
+print(10 // 2) # 5
+print(10 // 7) # 1
+print(10 // 5) # 2
