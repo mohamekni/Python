5d8bae4d682e2353d466e8869f8149f3ff2a53b3
mohamekni <mekni.moha99@gmail.com>
2025-05-29 23:54:25 +0200

    Learn Python

---
+++ 022 - Tuples And Methods Part One.py
@@ -0,0 +1,22 @@
+# Tuples
+
+# [1] Tuples Items Are Enclosed in Parentheses
+# [2] You Can Remove The Parentheses If You Want
+# [3] Tuples Are Ordered, To Use Index To Access Item
+# [4] Tuples Are Immutable => You Can't Add or Delete
+# [5] Tuples Items Is Not Unique
+# [6] Tuples Can Have Different Data Types
+
+
+
+myTupleOne = ("Mohamed" , "Mekni")
+myTupleTwo = "Ademo" , "NOS"
+print(type(myTupleOne)) # <class 'tuple'>
+print(type(myTupleTwo)) # <class 'tuple'>
+
+
+# Tuple Indexing
+myTupleThree = (1,2,3,4,5)
+print(myTupleThree[0]) # 1
+print(myTupleThree[-1]) # 5
+print(myTupleThree[-3]) # 3
