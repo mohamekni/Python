0180c9453670ba47e6e93ee982433bc216c1af17
mohamekni <mekni.moha99@gmail.com>
2025-06-08 04:10:29 +0200

    Learn Python

---
+++ 041 - Control Flow - Ternary Conditional Operator.py
@@ -0,0 +1,15 @@
+# Ternary Conditional Operator
+
+
+Games = "League Of Legends"
+if Games == "Valorant": print("You do not have permission to attend our party")
+elif Games == "League Of Legends" : print("Welcome")
+
+
+# Short If
+movieRate = 18
+age = 17
+
+# if age < movieRate : print("Movie S Not Good For U")
+# elif age > movieRate : print("Movie S Good For U")
+print("Movie Is Good For U" if age > movieRate else "Movie S Not Good For U")
