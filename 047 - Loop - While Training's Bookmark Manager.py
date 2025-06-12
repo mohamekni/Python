3d9b8c30ab8fe2de41c1800a5f19f52469c43528
mohamekni <mekni.moha99@gmail.com>
2025-06-12 23:27:03 +0200

    Learn Python

---
+++ 047 - Loop - While Training's Bookmark Manager.py
@@ -0,0 +1,26 @@
+# Loop => While Training
+
+
+# Empty List To Fill 
+myFavoriteGames = [
+
+]
+
+# Max Games
+maxGames = 5
+
+while maxGames > 0 :
+
+    myFavoriteGames.append(input('Tapez Votre Game: ').capitalize().lower())
+    print(myFavoriteGames)
+    print(f'Game Added, {maxGames - 1} Places Left')
+    maxGames -= 1
+
+else : 
+    print('You List Is Full')
+
+myFavoriteGames.sort()
+index = 0
+while index < len(myFavoriteGames) :
+    print(f"{index + 1} {myFavoriteGames[index]}")
+    index += 1
