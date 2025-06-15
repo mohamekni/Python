70d0ce749a6fd4269a13149a74e77788bf5d2201
mohamekni <mekni.moha99@gmail.com>
2025-06-15 03:02:33 +0200

    Learn Python

---
+++ 049 - Loop For and Else.py
@@ -0,0 +1,19 @@
+# Loop For
+
+
+myNumber = [1,2,3,4,5,6,7,8,9]
+
+for i in myNumber:
+
+    if i % 2 == 0 :
+
+        print(f"This Number {i} Is Even")
+
+    else :
+
+        print(f"This Number {i} Is Odd")
+
+name = "MohaLaSquale"
+for i in name :
+    
+    print(f"{i.upper()}")
