c2c7c2c9c092688221cb46e355abc1276870da9b
mohamekni <mekni.moha99@gmail.com>
2025-06-20 03:16:55 +0200

    Learn Python

---
+++ 057 - Function Default Parameters.py
@@ -0,0 +1,9 @@
+# Function Default Parameters
+
+
+def say_hello(name,age,country="Unknown"):
+    print(f"Hello {name} Your Age {age} and Your Country Is {country}")
+
+say_hello("Mohamed",22,"La Banane")
+say_hello("Ademo",33,"Tarterts")
+say_hello("Ademo", 33)
