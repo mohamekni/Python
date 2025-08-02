0e37d58597879bf90bb91d061769201d30f655f2
mohamekni <mekni.moha99@gmail.com>
2025-08-02 20:18:38 +0200

    Learn Python

---
+++ 071 - Built In Functions Part 5 Filter.py
@@ -0,0 +1,37 @@
+# Built In Function => Filter
+
+# [1] Filter Take A Function + Iterator
+# [2] Filter Run A Function On Every Element
+# [3] The Function Can Be Pre-Defined Function or Lambda Function
+# [4] Filter Out All Elements For Which The Function Return True
+# [5] The Function Need To Return Boolean Value
+
+# Example 1 
+
+def checkNumber(num):
+    if num > 20:
+        return num
+    
+myNumbers = [10, 20, 30, 40, 50, 60, 70]
+result = filter(checkNumber, myNumbers)
+for number in result:
+    print(number)
+
+
+# Example 2
+
+def checkName(name):
+    if name.startswith('M'):
+        return name
+    
+myNames = ["Mohamed", "Polak" , "Moha"]
+result = filter(checkName, myNames)
+for name in result:
+    print(name)
+
+
+# Example 3
+    
+myTexts = ["Mohamed", "Polak" , "Moha"]
+for name in filter(lambda name: name.startswith('M'), myTexts):
+    print(name)
